2051343e56996d8f287bb00f909ffbb163c62abe
Kah Siong Tan <jax79sg@yahoo.com.sg>
2020-02-25 10:39:28 +0800

    Update extractpypi.py

--- pypi/extractpypi.py
+++ pypi/extractpypi.py
@@ -1,4 +1,14 @@
+# Purpose of this module
+# When you used bandersnatch to download packages, they are primarily saved in 2 spots.
+# First, a html named after the package is created in the 'simple' folder, this html will list the paths to the actual versioned packages
+# Second, a series of indepth folders are created to store the actual version packages in the packaged folder.
+# If you just want to selectively take out some packages, it is humanely difficult due to the complex folder structure. 
+# This module is developed to handled the last point.
 
+# How to use
+# Assuming that you only need torch and torchvision, then run the following.
+# extractpypi(['torch','torchvision']) 
+```
 import os
 import tqdm
 import shutil
