8ed684dcf023451552cb5db26aee44f334a1a28e
Kah Siong Tan <jax79sg@yahoo.com.sg>
2020-02-26 13:42:59 +0800

    Added SSH cap

--- pypi/extractpypi.py
+++ pypi/extractpypi.py
@@ -7,28 +7,36 @@
 
 # How to use
 # Assuming that you only need torch and torchvision, then run the following.
-# extractpypi(['torch','torchvision']) 
+# extractpypi(['torch','torchvision'],ssh="user/password/123.22.112.2") 
 ```
 import os
 import tqdm
 import shutil
 
-def extractpypi(packagelist=[], sourceIndex='/mnt/1a216e0c-9bdc-44ac-8f89-bf2cb3efcfd8/repos/pypi6/web/simple/', sourcePackages='/mnt/1a216e0c-9bdc-44ac-8f89-bf2cb3efcfd8/repos/pypi6/web/packages/', dest='/media/user/BIG_WHITE_4/pypi6'):
+def extractpypi(packagelist=[], sourceIndex='/mnt/1a216e0c-9bdc-44ac-8f89-bf2cb3efcfd8/repos/pypi6/web/simple/', sourcePackages='/mnt/1a216e0c-9bdc-44ac-8f89-bf2cb3efcfd8/repos/pypi6/web/packages/', dest='/media/user/BIG_WHITE_4/pypi6', ssh=None):
     with tqdm.tqdm(total=len(packagelist)) as pbar:
         for package in packagelist:
-            listOfPackages=processIndexHtmls(package, sourceIndex, dest)
-            processPackages(listOfPackages,sourcePackages, dest)
+            listOfPackages=processIndexHtmls(package, sourceIndex, dest, ssh=ssh)
+            processPackages(listOfPackages,sourcePackages, dest, ssh=ssh)
             pbar.update()
 
-def processIndexHtmls(package, sourceIndex, dest):
+def processIndexHtmls(package, sourceIndex, dest, ssh):
     #Copy it to dest
     indexpath=sourceIndex+package
     indexhtml=sourceIndex+package+'/index.html'
-    try:
-        result = shutil.copytree(indexpath, dest+'/simple/'+package)
-        # print("Copy completed in {}".format(result))
-    except FileExistsError:
-        print("File {} exists, skip!".format(dest+'/simple/'+package))
+    if (ssh is not None):
+        user=ssh.split("/")[0]
+        password=ssh.split("/")[1]
+        host==ssh.split("/")[2]
+        import subprocess
+        subprocess.run(["scp", indexpath, user+"@"+host+":"+dest+'/simple/'+package])
+#         subprocess.run(["scp", "foo.bar", "joe@srvr.net:/path/to/foo.bar"])
+    else:
+        try:
+            result = shutil.copytree(indexpath, dest+'/simple/'+package)
+            # print("Copy completed in {}".format(result))
+        except FileExistsError:
+            print("File {} exists, skip!".format(dest+'/simple/'+package))
 
 
     #Get the index.html
@@ -46,25 +54,33 @@ def processIndexHtmls(package, sourceIndex, dest):
     return packagesLinks
 
 
-def processPackages(listOfPackages,sourcePackages, dest):
+def processPackages(listOfPackages,sourcePackages, dest, ssh=None):
     #Copy the entire path of packges into dest
     for package in listOfPackages:
         subDir = package.split("/")
         subDir=subDir[:-1]
         currentDir=dest + '/packages'
-        try:
-            os.mkdir(currentDir)
-        except FileExistsError:
-            pass
-
-        for dir in subDir:
-            currentDir=currentDir+"/"+dir
+        
+        if (ssh is not None):
+            user=ssh.split("/")[0]
+            password=ssh.split("/")[1]
+            host==ssh.split("/")[2]
+            import subprocess
+            subprocess.run(["scp", sourcePackages+package, user+"@"+host+":"+dest + '/packages/'+package])
+        else:        
             try:
                 os.mkdir(currentDir)
             except FileExistsError:
                 pass
-        result = shutil.copy(sourcePackages+package, dest + '/packages/'+package)
-        print("Copy completed in {}".format(result))
+
+            for dir in subDir:
+                currentDir=currentDir+"/"+dir
+                try:
+                    os.mkdir(currentDir)
+                except FileExistsError:
+                    pass
+            result = shutil.copy(sourcePackages+package, dest + '/packages/'+package)
+            print("Copy completed in {}".format(result))
     pass
 
 extractpypi(['torch','torchvision'])
